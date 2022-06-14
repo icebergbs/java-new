33b097fec4ed474bf98f9b0bebef7d46201a189a
geky <geky@esupplychain.com>
2022-06-14 14:01:51 +0800

    java9

---
+++ java9test/src/com/entity/TestModule.java
@@ -0,0 +1,16 @@
+package com.entity;
+
+
+import com.java.bean.Persion;
+
+/**
+ * @author bingshan
+ * @date 2022/5/6 14:21
+ */
+public class TestModule {
+    public static void main(String[] args) {
+        Persion p = new Persion();
+        p.setName("Module");
+        System.out.println(p.toString());
+    }
+}
