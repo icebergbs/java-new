26cf1a98c95a0e1ccde64024f3df0c808e9151c1
geky <geky@esupplychain.com>
2022-06-15 12:29:25 +0800

    java12

---
+++ src/main/java12/Java12.java
@@ -0,0 +1,22 @@
+package main.java12;
+
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * @author bingshan
+ * @date 2022/5/6 13:44
+ */
+public class Java12 {
+
+    public static void main(String[] args) {
+
+        /** 1、JShell */
+
+
+
+
+    }
+
+}
