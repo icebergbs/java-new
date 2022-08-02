c4c8f953414895ab7ac585bf8179bd36fb959885
thuliumkyg <1299338908@qq.com>
2022-08-02 22:21:58 +0800

    algorithm

---
+++ src/com/algorithm/codinginterviewguide/chapter1/C09_3x_最大值减去最小值小于或等于num的子数组数量.java
@@ -0,0 +1,27 @@
+package com.algorithm.codinginterviewguide.chapter1;
+
+
+import java.util.Stack;
+
+/**
+ * 题目:
+ *    给定数组arr 和 整数num, 共返回有多少个子数组满足如下情况:
+ *      max(arr[i..j]) - min(arr[i..j]) <= num
+ *      max(arr[i..j]) 表示子数组arr[i..j] 中的最大值, min(arr[i..j]) 表示子数组arr[i..j]中的最小值
+ *
+ *  要求:
+ *      如果数组长度为N, 请实现时间复杂度为O(N) 的解法
+ */
+public class C09_3x_最大值减去最小值小于或等于num的子数组数量 {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
