73eec5e36e5104b91661d9798b52d25e6b1ff827
geky <geky@esupplychain.com>
2022-10-13 12:58:16 +0800

    algorithm

--- src/com/algorithm/codinginterviewguide/chapter2/C07_1x_判断一个链表是否为回文结构.java
+++ src/com/algorithm/codinginterviewguide/chapter2/C07_1x_判断一个链表是否为回文结构.java
@@ -176,7 +176,7 @@ rightStart节点的值，看是否一样。
  */
 
     /**
-     * 方法三
+     *   11个中间变量  - 垃圾
      * @param head
      * @return
      */
@@ -228,6 +228,55 @@ rightStart节点的值，看是否一样。
         mid.next = rNext;
 
         return true;
+    }
+
+    /**
+     * 方法三
+     *      用到三个中间变量
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome3(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node n1 = head;
+        Node n2 = head;
+
+        while (n2.next != null && n2.next.next != null) { //查找中间节点
+            n1 = n1.next;  // n1 -> 中部
+            n2 = n2.next.next; // n2 -> 结尾
+        }
+        n2 = n1.next;
+        n1.next = null;
+        Node n3 = null;
+        while (n2 != null) {
+            n3 = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = n3;
+        }
+        n3 = n1;
+        n2 = head;
+        boolean res = true;
+        while (n1 != null && n2 != null) {
+            if (n2.value != n1.value) {
+                res = false;
+                return res;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        n1 = n3.next;
+        n3.next = null;
+        while (n1 != null) {
+            n2 = n1.next;
+            n1.next = n3;
+            n3 = n1;
+            n1 = n2;
+        }
+        return res;
+
     }
 
 }
