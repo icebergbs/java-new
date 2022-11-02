e911e78a07582174ad053ca63d143d95878c23a1
geky <geky@esupplychain.com>
2022-11-02 15:00:13 +0800

    algorithm

--- src/com/algorithm/codinginterviewguide/chapter2/C11_4x_两个单链表相交的一系列问题.java
+++ src/com/algorithm/codinginterviewguide/chapter2/C11_4x_两个单链表相交的一系列问题.java
@@ -44,8 +44,88 @@ public class C11_4x_两个单链表相交的一系列问题 {
   3. 如果链表有环，那么fast指针和slow指针一定会在环中的某个位置相遇，当fast和slow相遇时，
 fast指针重新回到head的位置，slow指针不动。 接下来，fast指针从每次移动两步改为每次移动一步，
 slow指针依然每次移动一步，然后继续遍历。
-  4.
-  
+  4. fast指针和slow指针一定会再次相遇，并且在第一个入环的节点处相遇。 证明略。
+
  */
+
+    /**
+     * 问题一 实现
+     * @param head
+     * @return
+     */
+    public Node getLoopNode(Node head) {
+        if (head == null || head .next == null || head.next.next == null) {
+            return null;
+        }
+
+        Node n1 = head.next;
+        Node n2 = head.next.next;
+        while (n1 != n2) {
+            if (n2.next == null || n2.next.next == null) {
+                return null;
+            }
+            n2 = n2.next.next;
+            n1 = n1.next;
+        }
+        n2 = head;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
+
+    }
+
+/*
+  两个链表都无环
+  问题二： 如何判断两个无环链表是否相交，相交则返回第一个相交节点，不相交则返回null.
+  如果两个无环链表相交，那么从相交节点开始，一直到两个链表终止的这一段，是两个链表共享的。
+  1. 链表1从头节点开始，走到最后一个节点(不是结束)， 统计链表1的长度记为len1, 同时
+记录链表1的最后一个节点记为end1.
+  2. 链表2从头节点开始，走到最后一个节点(不是结束)， 统计链表2的长度记为len2, 同时
+记录链表2的最后一个节点记为end2.
+  3. 如果 end1 != end2, 说明两个链表不相交，返回null即可； 如果end1 == end2,
+说明两个链表相交，进入步骤4寻找第一个相交节点
+  4. 如果链表1比较长，链表1就先走 len1-len2 步；然后两个链表一起走，一起走的过程中，
+两个链表第一次走到一起的那个节点就是第一个相交的节点。
+
+ */
+
+    /**
+     * 问题二 实现
+     * @param head1
+     * @return
+     */
+    public Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        if (cur1 != cur2) {
+            return null;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
 
 }
