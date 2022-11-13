39d1880c58bc0cbdbe1a6319a617e067d7500af6
geky <geky@esupplychain.com>
2022-11-13 17:11:09 +0800

    design

---
+++ src/com/design/oop/polymorphism/case3/A.java
@@ -0,0 +1,9 @@
+package com.design.oop.polymorphism.case3;
+
+/**
+ * @author bingshan
+ * @date 2022/11/13 9:36
+ */
+public interface A {
+    void a();
+}
---
+++ src/com/design/oop/polymorphism/case3/C.java
@@ -0,0 +1,12 @@
+package com.design.oop.polymorphism.case3;
+
+/**
+ * @author bingshan
+ * @date 2022/11/13 9:36
+ */
+public class C implements B , A {
+    @Override
+    public void a() {
+
+    }
+}
