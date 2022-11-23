c9d0fe42f4284e64e8c27f37f899d8cb5e9e7a4a
geky <geky@esupplychain.com>
2022-11-23 20:04:59 +0800

    algorithm

--- src/com/algorithm/codinginterviewguide/chapter2/C13_1x_删除无序单链表中值重复出现的节点.java
+++ src/com/algorithm/codinginterviewguide/chapter2/C13_1x_删除无序单链表中值重复出现的节点.java
@@ -1,5 +1,7 @@
 package com.algorithm.codinginterviewguide.chapter2;
 
+import java.util.HashSet;
+
 /**
  * 题目:
  *   给定一个无序单链表的头节点head, 删除其中值重复出现的节点。
@@ -25,6 +27,80 @@ public class C13_1x_删除无序单链表中值重复出现的节点 {
         }
     }
 
+/*
+    方法一： 利用哈希表，事件复杂度为O(N), 额外空间复杂度为O(N)
+
+ */
+    public void removeRep1(Node head) {
+        if (head == null) {
+            return;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        Node pre = head;
+        Node cur = head.next;
+        set.add(head.value);
+        while (cur != null) {
+            if (set.contains(cur.value)) {
+                pre.next = cur.next;
+            } else {
+                set.add(cur.value);
+                pre = cur;
+
+            }
+            cur = cur.next;
+        }
+    }
+
+/*
+    方法二： 类似选择排序， 时间复杂度为O(N*N)，空间复杂度为O(1).
+    例如链表： 1->2->3->4->4->2->2->1->null
+    首先是头节点，节点为1，往后检查所有节点值为1的节点，全部删除，链表变为：  1->2->3->4->4->2->2->null
+    。。。。。。
+ */
+    public void removeRep2(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node pre = null;
+        Node next = null;
+        while (cur != null) {
+            pre = cur;
+            next = cur.next;
+            while (next != null) {
+                if (cur.value == next.value) {
+                    pre.next = next.next;
+                } else {
+                    pre = next;
+                }
+                next = next.next;
+            }
+            cur = cur.next;
+        }
+    }
+
+    public void removeRep2xx(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node pre = head;
+        Node cur = head.next;
+        while (cur != null) {
+            Integer value = pre.value;
+            Node pre2 = pre;
+            Node cur2 = cur;
+            while (cur2 != null) {
+                if (cur2.value == value) {
+                    pre2.next = cur2.next;
+                } else {
+                    pre2 = cur2;
+                }
+                cur2 = cur2.next;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+    }
 
 
 }
